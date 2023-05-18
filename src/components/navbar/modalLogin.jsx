2a3507fc36214231989ff838903d654cd893447e
Daniel Curvello <curvellodaniel@gmail.com>
2023-05-18 20:53:10 -0300

    melhorias commit 8

--- src/components/navbar/modalLogin.jsx
+++ src/components/navbar/modalLogin.jsx
@@ -1,6 +1,6 @@
 import Modal from 'react-modal';
 import React from 'react';
-import './modalLogin.css'
+
 import { MdClose } from 'react-icons/md';
 
 
@@ -9,13 +9,12 @@ export function ModalLogin({ isOpen, closeModal }) {
 
     const modalStyle = {
         content: {
-          width: '60%', /* Ajuste o valor conforme necessário */
-          maxWidth: '500px', /* Ajuste o valor conforme necessário */
+          width: '80%', /* Ajuste o valor conforme necessÃ¡rio */
+          maxWidth: '500px', /* Ajuste o valor conforme necessÃ¡rio */
           borderRadius: '10px',
-          boxShadow: '0px 1px 8px 2px rgba(0, 0, 0, 0.5)',
-          
+          boxShadow: '0px 1px 8px 2px rgba(0, 0, 0, 0.5)',          
           border: 'none',
-          margin: '20vh auto',
+          margin: '16vh auto',
         },
 
         overlay: {
@@ -39,7 +38,17 @@ export function ModalLogin({ isOpen, closeModal }) {
         fontSize: '24px',
         fontWeight: 600,
         paddingBottom: '25px',
-        margin: '24px 0px 12px',
+        margin: '-14px 0px 1px',
+      };
+
+      const h2Style = {
+        color: 'rgb(143 143 143)',
+        textAlign: 'center',
+        fontSize: '15px',
+        fontWeight: 600,
+        paddingBottom: '25px',
+        margin: '-14px 0px 12px',
+        marginTop: '37px'
       };
 
       const closeButtonStyle = {
@@ -49,10 +58,30 @@ export function ModalLogin({ isOpen, closeModal }) {
         background: 'none',
         border: 'none',
         cursor: 'pointer',
-        fontSize: '20px',
+        fontSize: '26px',
         color: 'rgba(0, 0, 0, 0.5)',
         padding: '0',
       };
+
+      const googleButtonStyle = {
+        background: '#4285F4',
+        borderRadius: '20px',
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        cursor: 'pointer',
+        fontSize: '20px'
+      };
+
+      const emailInputStyle = {
+        border: '2px solid #4285F4',
+        borderRadius: '4px',
+        padding: '10px',
+        marginBottom: '10px',
+        width: '60%',
+      };
+      
+      
 
 
       return (
@@ -63,10 +92,22 @@ export function ModalLogin({ isOpen, closeModal }) {
           style={modalStyle}
         >
           <div style={{ background: 'white' }}>
+            
             <button style={closeButtonStyle} onClick={closeModal}>
               <MdClose />
             </button>
+            
             <h1 style={h1Style}>Login</h1>
+            <div style={{ textAlign: 'center' }}>
+            <button style={googleButtonStyle}>Entrar com o Google</button>
+            </div>
+            <div>
+            <h2 style={h2Style}>OU</h2>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              <input type="text" placeholder="Email" style={emailInputStyle}/>
+              <input type="password" placeholder="Senha" style={emailInputStyle} />
+            </div>
           </div>
         </Modal>
       );
